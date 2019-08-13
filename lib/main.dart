3726e91cf3aa69fd1f85dee0d36a4481ba1c6278
ak052142 <anveshkumar.kolluri@cerner.com>
2019-08-13 14:34:15 -0500

    Final mi card with email and phone number

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,67 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-//            crossAxisAlignment: CrossAxisAlignment.stretch,
-//            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage("images/Pio.png"),
               ),
               Text(
-                'Rich',
+                'Richest One',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'OWNS EVERYTHING',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 50.0,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100, height: 0.0),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '222-222-2222',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'richricher@richest.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0),
+                  ),
+                ),
+              )
             ],
           ),
         ),
